57ce5b175661c62d189e519b8b0991eb295f61d6
compergames <comper@kosmiczni.pl>
2021-04-14 19:57:40 +0200

    sync

--- generate.php
+++ generate.php
@@ -147,94 +147,107 @@ ini_set('max_execution_time', 300000);
 		return $string;
 	}
 	// SAVE FILE
+	function generate_file($lang,$name_correctly)
+	{
+		$correct_names=array('en'=>'lang_en-US','es'=>'lang_es-AR','pt'=>'lang_pt-BR','fr'=>'lang_fr-FR','th'=>'lang_th-TH','pl'=>'lang_pl-PL');
+		if ($handle = opendir('./'.$lang.'/id/')) {
+			$sections=array();
+			while (false !== ($entry = readdir($handle))) {
+				if ($entry != "." && $entry != "..") {
+					$file=explode('.',$entry);
+					$sections[]=$file[0];
+				}
+			}
+			closedir($handle);
+			$array=array();
+			
+			$binary='';
+			$binary.=saveInt32(1196310860); //magic
+			$binary.=saveInt32(1); //version
+			$binary.=saveInt32(1548038144); //timestamp
+			$sections=array('ai','skill','data_recipe','map','data_quest','common','ui','age','photobook','title','arena','countbirds','combat','data_treasures','policy','data_text','daily',
+			'item','error','data_item','quest','interface','questionaire','data_monster','data_npc','login','data_config','pet','miscs','help','achievement','config','lottery','question','animation',
+			'social','data_equip','dungeon');
+			$important_characters=array('%','&','$','{','}','%s','%d');
+			$ic=count($important_characters);
+			$cnt=count($sections);
+			$binary.=saveInt16($cnt);
+			for($s=0;$s<$cnt;$s++){
+				$sec=array();
+				$sec['unk']=3176595456;
+				$sec['bundle_name']=$sections[$s];
+				if($sec['bundle_name']!='ai') $sec['unk']=3176644077;
+				$binary.=saveInt32($sec['unk']);
+				$binary.=saveString($sec['bundle_name']);
+				$sec['bundle_count']=0;
+				$sec['i18n']=array();
+				$fn = fopen('./'.$lang.'/id/'.$sec['bundle_name'].'.txt',"r");
+				while(! feof($fn))  {
+					$result=str_replace("\n",'',fgets($fn));
+					if(!$result) continue;
+					$result = explode("\t",$result);
+					$sec['i18n'][(int)$result[0]]=array('id'=>(string)$result[1]);
+					if(count($result)<2) echo 'TAB ERROR ID ['.$sec['bundle_name'].' id: '.$result[0].']<br />';
+					$sec['bundle_count']++;
+				}
+				fclose($fn);
+				$fn = fopen('./'.$lang.'/source/'.$sec['bundle_name'].'.txt',"r");
+				while(! feof($fn))  {
+					$result=str_replace("\n",'',fgets($fn));
+					if(!$result) continue;
+					$result = explode("\t",$result);
+					if(count($result)<2) echo 'TAB ERROR SOURCE ['.$sec['bundle_name'].' id: '.$result[0].']<br />';
+					$sec['i18n'][(int)$result[0]]['source']=((string)$result[1]);
+				}
+				fclose($fn);
+				$fn = fopen('./'.$lang.'/target/'.$sec['bundle_name'].'.txt',"r");
+				$l=0;
+				while(! feof($fn))  {
+					//$result=str_replace("\r\n",'',fgets($fn));
+					$result=str_replace("\n",'',fgets($fn));
+					if(!$result) continue;
+					$result = explode("\t",$result);
+					$id=(int)$result[0];
+					if(count($result)<2) echo 'TAB ERROR TARGET ['.$sec['bundle_name'].' id: '.$id.']<br />';
+					if($l!=$id) echo 'INDEX ERROR TARGET ['.$sec['bundle_name'].' id: '.$l.'/'.$id.']<br />';
+					$sec['i18n'][$id]['target']=((string)$result[1]);
+					$sec['i18n'][$id]['target']=str_replace("\n",'',$sec['i18n'][$id]['target']);
+					$sec['i18n'][$id]['target']=str_replace("\r",'',$sec['i18n'][$id]['target']);
+					for($c=0;$c<$ic;$c++){
+						$c1=substr_count($sec['i18n'][$id]['source'], $important_characters[$c]);
+						$c2=substr_count($sec['i18n'][$id]['target'], $important_characters[$c]);
+						if($c1 != $c2) echo 'TAB '.$sec['bundle_name'].' ID: '.$id.': <b>'.$important_characters[$c].'</b> count invalid ('.$c2.'/'.$c1.')<br />';
+					}
+					
+					$l++;
+				}
+				fclose($fn);
+				$binary.=saveInt32($sec['bundle_count']);
+				for($i=0;$i<$sec['bundle_count'];$i++){
+					$binary.=saveString($sec['i18n'][$i]['id']);
+					$binary.=saveString(unespaceString($sec['i18n'][$i]['source']));
+					$binary.=saveString(unespaceString($sec['i18n'][$i]['target']));
+				}
+				$array[]=$sec;
+				echo $sec['bundle_name'].' done...<br />';
+			}
+			$binary.=saveInt16(48621);
+			$name = 'lang_DATA.data';
+			if($name_correctly) $name=$correct_names[$lang];
+			if($file = fopen('./'.$name.'.data', 'wb')){
+				fwrite($file, $binary);
+				fclose($file);
+			}
+			
+		}
+	}
 	$lang='en';
 	if(isset($_GET['lang'])) $lang=$_GET['lang'];
-	if ($handle = opendir('./'.$lang.'/id/')) {
-		$sections=array();
-		while (false !== ($entry = readdir($handle))) {
-			if ($entry != "." && $entry != "..") {
-				$file=explode('.',$entry);
-				$sections[]=$file[0];
-			}
-		}
-		closedir($handle);
-		$array=array();
-		
-		$binary='';
-		$binary.=saveInt32(1196310860); //magic
-		$binary.=saveInt32(1); //version
-		$binary.=saveInt32(1548038144); //timestamp
-		$sections=array('ai','skill','data_recipe','map','data_quest','common','ui','age','photobook','title','arena','countbirds','combat','data_treasures','policy','data_text','daily',
-		'item','error','data_item','quest','interface','questionaire','data_monster','data_npc','login','data_config','pet','miscs','help','achievement','config','lottery','question','animation',
-		'social','data_equip','dungeon');
-		$important_characters=array('%','&','$','{','}','%s','%d');
-		$ic=count($important_characters);
-		$cnt=count($sections);
-		$binary.=saveInt16($cnt);
-		for($s=0;$s<$cnt;$s++){
-			$sec=array();
-			$sec['unk']=3176595456;
-			$sec['bundle_name']=$sections[$s];
-			if($sec['bundle_name']!='ai') $sec['unk']=3176644077;
-			$binary.=saveInt32($sec['unk']);
-			$binary.=saveString($sec['bundle_name']);
-			$sec['bundle_count']=0;
-			$sec['i18n']=array();
-			$fn = fopen('./'.$lang.'/id/'.$sec['bundle_name'].'.txt',"r");
-			while(! feof($fn))  {
-				$result=str_replace("\n",'',fgets($fn));
-				if(!$result) continue;
-				$result = explode("\t",$result);
-				$sec['i18n'][(int)$result[0]]=array('id'=>(string)$result[1]);
-				if(count($result)<2) echo 'TAB ERROR ID ['.$sec['bundle_name'].' id: '.$result[0].']<br />';
-				$sec['bundle_count']++;
-			}
-			fclose($fn);
-			$fn = fopen('./'.$lang.'/source/'.$sec['bundle_name'].'.txt',"r");
-			while(! feof($fn))  {
-				$result=str_replace("\n",'',fgets($fn));
-				if(!$result) continue;
-				$result = explode("\t",$result);
-				if(count($result)<2) echo 'TAB ERROR SOURCE ['.$sec['bundle_name'].' id: '.$result[0].']<br />';
-				$sec['i18n'][(int)$result[0]]['source']=((string)$result[1]);
-			}
-			fclose($fn);
-			$fn = fopen('./'.$lang.'/target/'.$sec['bundle_name'].'.txt',"r");
-			$l=0;
-			while(! feof($fn))  {
-				//$result=str_replace("\r\n",'',fgets($fn));
-				$result=str_replace("\n",'',fgets($fn));
-				if(!$result) continue;
-				$result = explode("\t",$result);
-				$id=(int)$result[0];
-				if(count($result)<2) echo 'TAB ERROR TARGET ['.$sec['bundle_name'].' id: '.$id.']<br />';
-				if($l!=$id) echo 'INDEX ERROR TARGET ['.$sec['bundle_name'].' id: '.$l.'/'.$id.']<br />';
-				$sec['i18n'][$id]['target']=((string)$result[1]);
-				$sec['i18n'][$id]['target']=str_replace("\n",'',$sec['i18n'][$id]['target']);
-				$sec['i18n'][$id]['target']=str_replace("\r",'',$sec['i18n'][$id]['target']);
-				for($c=0;$c<$ic;$c++){
-					$c1=substr_count($sec['i18n'][$id]['source'], $important_characters[$c]);
-					$c2=substr_count($sec['i18n'][$id]['target'], $important_characters[$c]);
-					if($c1 != $c2) echo 'TAB '.$sec['bundle_name'].' ID: '.$id.': <b>'.$important_characters[$c].'</b> count invalid ('.$c2.'/'.$c1.')<br />';
-				}
-				
-				$l++;
-			}
-			fclose($fn);
-			$binary.=saveInt32($sec['bundle_count']);
-			for($i=0;$i<$sec['bundle_count'];$i++){
-				$binary.=saveString($sec['i18n'][$i]['id']);
-				$binary.=saveString(unespaceString($sec['i18n'][$i]['source']));
-				$binary.=saveString(unespaceString($sec['i18n'][$i]['target']));
-			}
-			$array[]=$sec;
-			echo $sec['bundle_name'].' done...<br />';
-		}
-		$binary.=saveInt16(48621);
-		if($file = fopen('./lang_DATA.data', 'wb')){
-			fwrite($file, $binary);
-			fclose($file);
-		}
-		
+	if($lang == 'all'){
+		$all=array('en','es','pt','fr','pl','th');
+		$cnt =count($all);
+		for($i=0;$i<$cnt;$i++) generate_file($all[$i],true);
 	}
+	else generate_file($lang,false);
+	
 ?>
